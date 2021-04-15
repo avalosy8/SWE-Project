79b097785c2698c8e962804507bf99bde5a7e3c8
Yenessa <avalosmaldona.ye@ufl.edu>
2021-04-15 14:22:20 -0400

    added google calendar api, updated login ui
    login ui works better with sidebar menu

---
+++ src2/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Sidebar from "./Components/Sidebar";
+import Calendar2 from "./Components/Calendar2";
+import Main from "./Components/Main";
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar />
+      <Main />
+      <Calendar2 />
+    </div>
+  );
+}
+
+export default App;
